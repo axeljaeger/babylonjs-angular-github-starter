daf225ea8160d0a93f2774064b4b75cd0650bdf6
Axel Jäger <axeljaeger@gmail.com>
2026-05-06 15:51:13 +0200

    Use details for sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { type AfterViewInit, Component, model, signal, viewChild } from '@angular/core';
 import { BabylonCanvas } from './BabylonCanvas';
+import { type PresetColor, Sidebar } from './sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [BabylonCanvas],
+  imports: [BabylonCanvas, Sidebar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements AfterViewInit {
+  protected readonly babylon = viewChild.required(BabylonCanvas);
+  public readonly fps = signal(0);
+  protected readonly color = model<PresetColor>('red');
 
+  ngAfterViewInit(): void {
+    window.setInterval(() => this.fps.set(this.babylon().engine.getFps()), 1000);
+  }
 }
